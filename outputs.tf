178d613770af8866afaa6eedd9fa798c22b85023
Rohit Amarnath <rohit.amarnath@full360.com>
2019-06-13 01:01:28 -0400

    fix the output bug

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@
 locals {
   name_list  = "${compact(concat(split("${var.split_delimiter}",join("${var.split_delimiter}", aws_ssm_parameter.default.*.name)), split("${var.split_delimiter}",join("${var.split_delimiter}", data.aws_ssm_parameter.read.*.name))))}"
   value_list = "${compact(concat(split("${var.split_delimiter}",join("${var.split_delimiter}", aws_ssm_parameter.default.*.value)), split("${var.split_delimiter}",join("${var.split_delimiter}", data.aws_ssm_parameter.read.*.value))))}"
-  arn_list   = "${compact(concat(split("${var.split_delimiter}",join("${var.split_delimiter}", aws_ssm_parameter.default.*.value)), split("${var.split_delimiter}",join("${var.split_delimiter}", data.aws_ssm_parameter.read.*.arn))))}"
+  arn_list   = "${compact(concat(split("${var.split_delimiter}",join("${var.split_delimiter}", aws_ssm_parameter.default.*.arn)), split("${var.split_delimiter}",join("${var.split_delimiter}", data.aws_ssm_parameter.read.*.arn))))}"
 }
 
 output "names" {
@@ -21,6 +21,6 @@ output "map" {
 }
 
 output "arn_map" {
-  description = "A map of the names and arns created"
+  description = "A map of the names and ARNs created"
   value       = "${zipmap(local.name_list, local.arn_list)}"
 }
