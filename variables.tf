f2b8eb55f403836b44e4838b4ee9af0117896588
Josh Myers <joshuajmyers@gmail.com>
2019-08-15 10:33:11 +0100

    Upgrade to Terraform 0.12
    Run `terraform 0.12upgrade` against codebase. Note that due to Terraform
    0.12 now using sets, we call `tolist` to workaround [1]
    
    [1] https://github.com/terraform-providers/terraform-provider-aws/issues/7522

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "parameter_read" {
-  type        = "list"
+  type        = list(string)
   description = "List of parameters to read from SSM. These must already exist otherwise an error is returned. Can be used with `parameter_write` as long as the parameters are different."
   default     = []
 }
 
 variable "parameter_write" {
-  type = "list"
+  type = list(string)
 
   description = <<DESC
   List of maps with the Parameter values in this format.
@@ -20,29 +20,31 @@ variable "parameter_write" {
   }]
 DESC
 
+
   default = []
 }
 
 variable "tags" {
-  type        = "map"
-  default     = {}
+  type = map(string)
+  default = {}
   description = "Map containing tags that will be added to the parameters"
 }
 
 variable "kms_arn" {
-  type        = "string"
-  default     = ""
+  type = string
+  default = ""
   description = "The ARN of a KMS key used to encrypt and decrypt SecretString values"
 }
 
 variable "enabled" {
-  type        = "string"
-  default     = "true"
+  type        = bool
+  default     = true
   description = "Set to `false` to prevent the module from creating and accessing any resources"
 }
 
 variable "split_delimiter" {
-  type        = "string"
-  default     = "~^~"
+  type = string
+  default = "~^~"
   description = "A delimiter for splitting and joining lists together for normalising the output"
 }
+
