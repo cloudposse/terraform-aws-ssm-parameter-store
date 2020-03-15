e7b7e8456090af5aea0c8c1dbfed5a1b9e7f915e
bunchopunch <bunchopunch@mustacheforpresident.com>
2020-03-14 20:05:43 -0600

    fix Quoted type constraints are deprecated

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "parameter_read" {
-  type        = "list"
+  type        = list
   description = "List of parameters to read from SSM. These must already exist otherwise an error is returned. Can be used with `parameter_write` as long as the parameters are different."
   default     = []
 }
 
 variable "parameter_write" {
-  type = "list"
+  type = list
 
   description = <<DESC
   List of maps with the Parameter values in this format.
@@ -24,25 +24,25 @@ DESC
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map
   default     = {}
   description = "Map containing tags that will be added to the parameters"
 }
 
 variable "kms_arn" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "The ARN of a KMS key used to encrypt and decrypt SecretString values"
 }
 
 variable "enabled" {
-  type        = "string"
+  type        = string
   default     = "true"
   description = "Set to `false` to prevent the module from creating and accessing any resources"
 }
 
 variable "split_delimiter" {
-  type        = "string"
+  type        = string
   default     = "~^~"
   description = "A delimiter for splitting and joining lists together for normalising the output"
 }
