88fc389a026d9535e99e62a4aa291abcd013fbf6
bunchopunch <bunchopunch@mustacheforpresident.com>
2020-03-14 20:09:09 -0600

    fix Interpolation-only expressions are deprecated

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 data "aws_ssm_parameter" "read" {
-  count = "${var.enabled == "true" ? length(var.parameter_read) : 0}"
-  name  = "${element(var.parameter_read, count.index)}"
+  count = var.enabled == "true" ? length(var.parameter_read) : 0
+  name  = element(var.parameter_read, count.index)
 }
 
 resource "aws_ssm_parameter" "default" {
-  count           = "${var.enabled == "true" ? length(var.parameter_write) : 0}"
-  name            = "${lookup(var.parameter_write[count.index], "name")}"
-  description     = "${lookup(var.parameter_write[count.index], "description", lookup(var.parameter_write[count.index], "name"))}"
-  type            = "${lookup(var.parameter_write[count.index], "type", "SecureString")}"
-  key_id          = "${lookup(var.parameter_write[count.index], "type", "SecureString") == "SecureString" && length(var.kms_arn) > 0 ? var.kms_arn : ""}"
-  value           = "${lookup(var.parameter_write[count.index], "value")}"
-  overwrite       = "${lookup(var.parameter_write[count.index], "overwrite", "false")}"
-  allowed_pattern = "${lookup(var.parameter_write[count.index], "allowed_pattern", "")}"
-  tags            = "${var.tags}"
+  count           = var.enabled == "true" ? length(var.parameter_write) : 0
+  name            = lookup(var.parameter_write[count.index], "name")
+  description     = lookup(var.parameter_write[count.index], "description", lookup(var.parameter_write[count.index], "name"))
+  type            = lookup(var.parameter_write[count.index], "type", "SecureString")
+  key_id          = lookup(var.parameter_write[count.index], "type", "SecureString") == "SecureString" && length(var.kms_arn) > 0 ? var.kms_arn : ""
+  value           = lookup(var.parameter_write[count.index], "value")
+  overwrite       = lookup(var.parameter_write[count.index], "overwrite", "false")
+  allowed_pattern = lookup(var.parameter_write[count.index], "allowed_pattern", "")
+  tags            = var.tags
 }
