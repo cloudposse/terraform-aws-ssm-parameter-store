530e34cdb3744c05eadc11f2e5cd4eda51d1fb93
aknysh <andriy.knysh@gmail.com>
2020-05-07 23:00:12 -0400

    Convert to TF 0.12. Add tests. Add Codefresh test pipeline

--- main.tf
+++ main.tf
@@ -1,16 +1,23 @@
 data "aws_ssm_parameter" "read" {
-  count = "${var.enabled == "true" ? length(var.parameter_read) : 0}"
-  name  = "${element(var.parameter_read, count.index)}"
+  count = var.enabled ? length(var.parameter_read) : 0
+  name  = element(var.parameter_read, count.index)
 }
 
 resource "aws_ssm_parameter" "default" {
-  count           = "${var.enabled == "true" ? length(var.parameter_write) : 0}"
-  name            = "${lookup(var.parameter_write[count.index], "name")}"
-  description     = "${lookup(var.parameter_write[count.index], "description", lookup(var.parameter_write[count.index], "name"))}"
-  type            = "${lookup(var.parameter_write[count.index], "type", "SecureString")}"
-  key_id          = "${lookup(var.parameter_write[count.index], "type", "SecureString") == "SecureString" && length(var.kms_arn) > 0 ? var.kms_arn : ""}"
-  value           = "${lookup(var.parameter_write[count.index], "value")}"
-  overwrite       = "${lookup(var.parameter_write[count.index], "overwrite", "false")}"
-  allowed_pattern = "${lookup(var.parameter_write[count.index], "allowed_pattern", "")}"
-  tags            = "${var.tags}"
+  count = var.enabled ? length(var.parameter_write) : 0
+  name  = tolist(var.parameter_write)[count.index]["name"]
+
+  description = lookup(
+    tolist(var.parameter_write)[count.index],
+    "description",
+    tolist(var.parameter_write)[count.index]["name"]
+  )
+
+  type            = lookup(tolist(var.parameter_write)[count.index], "type", "SecureString")
+  tier            = lookup(var.parameter_write[count.index], "tier", "Standard")
+  key_id          = lookup(tolist(var.parameter_write)[count.index], "type", "SecureString") == "SecureString" && length(var.kms_arn) > 0 ? var.kms_arn : ""
+  value           = tolist(var.parameter_write)[count.index]["value"]
+  overwrite       = lookup(tolist(var.parameter_write)[count.index], "overwrite", "false")
+  allowed_pattern = lookup(tolist(var.parameter_write)[count.index], "allowed_pattern", "")
+  tags            = var.tags
 }
--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,61 @@
 # Splitting and joining, and then compacting a list to get a normalised list
 locals {
-  name_list  = "${compact(concat(split("${var.split_delimiter}",join("${var.split_delimiter}", aws_ssm_parameter.default.*.name)), split("${var.split_delimiter}",join("${var.split_delimiter}", data.aws_ssm_parameter.read.*.name))))}"
-  value_list = "${compact(concat(split("${var.split_delimiter}",join("${var.split_delimiter}", aws_ssm_parameter.default.*.value)), split("${var.split_delimiter}",join("${var.split_delimiter}", data.aws_ssm_parameter.read.*.value))))}"
+  name_list = compact(
+    concat(
+      split(
+        var.split_delimiter,
+        join(var.split_delimiter, aws_ssm_parameter.default.*.name)
+      ),
+      split(
+        var.split_delimiter,
+        join(var.split_delimiter, data.aws_ssm_parameter.read.*.name)
+      )
+    )
+  )
+
+  value_list = compact(
+    concat(
+      split(
+        var.split_delimiter,
+        join(var.split_delimiter, aws_ssm_parameter.default.*.value)
+      ),
+      split(
+        var.split_delimiter,
+        join(var.split_delimiter, data.aws_ssm_parameter.read.*.value)
+      )
+    )
+  )
+
+  arn_list = compact(
+    concat(
+      split(
+        var.split_delimiter,
+        join(var.split_delimiter, aws_ssm_parameter.default.*.arn)
+      ),
+      split(
+        var.split_delimiter,
+        join(var.split_delimiter, data.aws_ssm_parameter.read.*.arn)
+      )
+    )
+  )
 }
 
 output "names" {
-  value       = "${local.name_list}"
+  value       = local.name_list
   description = "A list of all of the parameter names"
 }
 
 output "values" {
   description = "A list of all of the parameter values"
-  value       = "${local.value_list}"
+  value       = local.value_list
 }
 
 output "map" {
   description = "A map of the names and values created"
-  value       = "${zipmap(local.name_list, local.value_list)}"
+  value       = zipmap(local.name_list, local.value_list)
+}
+
+output "arn_map" {
+  description = "A map of the names and ARNs created"
+  value       = zipmap(local.name_list, local.arn_list)
 }
