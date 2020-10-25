27cec2955abe9074fe477bed3f3b6eb045e426fb
Denis Baryshev <dennybaa@gmail.com>
2020-10-26 00:15:32 +0300

    update providers versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = ">= 0.12.0, < 0.14.0"
+  required_version = ">= 0.12.6, < 0.14"
 
   required_providers {
-    aws      = "~> 2.0"
-    template = "~> 2.0"
-    local    = "~> 1.2"
-    null     = "~> 2.0"
+    aws    = ">= 2.49, < 4.0"
+    random = "~> 2"
+    null   = "~> 2"
   }
 }
