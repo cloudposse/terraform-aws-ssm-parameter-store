8abacfe1281fe5059f89f55b651eb9839f91d8af
bmosz <wojciech.szychta@persgroep.net>
2021-03-12 12:54:09 +0100

    Flag sensitive values as described in terraform 0.14 upgrade

--- outputs.tf
+++ outputs.tf
@@ -48,11 +48,13 @@ output "names" {
 output "values" {
   description = "A list of all of the parameter values"
   value       = local.value_list
+  sensitive   = true
 }
 
 output "map" {
   description = "A map of the names and values created"
   value       = zipmap(local.name_list, local.value_list)
+  sensitive   = true
 }
 
 output "arn_map" {
