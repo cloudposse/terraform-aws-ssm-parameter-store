26e0bc1dab3df3ef59441c7df713a86e1716370e
Veronika Gnilitska <veronika.gnilitska@gmail.com>
2024-01-31 18:41:37 +0200

    chore: add overwrite argument back

--- main.tf
+++ main.tf
@@ -14,11 +14,17 @@ resource "aws_ssm_parameter" "default" {
   for_each = local.parameter_write
   name     = each.key
 
-  description     = each.value.description
-  type            = each.value.type
-  tier            = each.value.tier
-  key_id          = each.value.type == "SecureString" && length(var.kms_arn) > 0 ? var.kms_arn : ""
-  value           = each.value.value
+  description = each.value.description
+  type        = each.value.type
+  tier        = each.value.tier
+  key_id      = each.value.type == "SecureString" && length(var.kms_arn) > 0 ? var.kms_arn : ""
+  value       = each.value.value
+  # Note on the deprecation warning:
+  # Configurations expecting the standard update flow will need to keep overwrite = true set
+  # until this becomes the default behavior in v6.0.0. Removing it in v5.X will result in
+  # the default value of false, preventing the parameter value from being updated.
+  # Source: https://github.com/hashicorp/terraform-provider-aws/issues/25636#issuecomment-1623661159
+  overwrite       = each.value.overwrite
   allowed_pattern = each.value.allowed_pattern
   data_type       = each.value.data_type
 
